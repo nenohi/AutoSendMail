519ef1d11b5e6d469b11aca63d3e4415c0c122f8
nenohi <kimutipartylove@gmail.com>
2021-06-17 10:32:59 +0900

    起動時のメッセージを1度だけにし、メールの自動送信状態を保存

--- Program.cs
+++ Program.cs
@@ -14,16 +14,28 @@ namespace AutoSendMail
         [STAThread]
         static void Main()
         {
+            if(!Properties.Settings.Default.MessageChecked)
+            {
+
             if (MessageBox.Show("このソフトを利用するにあたり\n誤送信などの問題が発生しても作成側は責任を持ちません。\nすべて自己責任での使用をお願い致します。", "利用規約", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                    Properties.Settings.Default.MessageChecked = true;
+                Application.SetHighDpiMode(HighDpiMode.SystemAware);
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new AutoSendMail());
+                }
+                else
+                {
+                    Application.Exit();
+                }
+            }
+            else
             {
                 Application.SetHighDpiMode(HighDpiMode.SystemAware);
                 Application.EnableVisualStyles();
                 Application.SetCompatibleTextRenderingDefault(false);
                 Application.Run(new AutoSendMail());
-            }
-            else
-            {
-                Application.Exit();
             }
         }
     }
